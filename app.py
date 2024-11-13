d9501dc6b38186c307ee963285d6a9a0ea363cd4
willt08 <85506557+willt08@users.noreply.github.com>
2024-11-13 14:25:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def callback():
     return 'Callback received!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
